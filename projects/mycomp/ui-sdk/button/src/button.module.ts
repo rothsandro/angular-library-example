33d22ea60b15563ab94edb985d49594778756564
Sandro Roth <sandroroth@outlook.com>
2022-03-06 12:40:04 +0100

    Add dependency between entry points

--- projects/mycomp/ui-sdk/button/src/button.module.ts
+++ projects/mycomp/ui-sdk/button/src/button.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button.component';
+import { I18nModule } from '@mycomp/ui-sdk/i18n';
 
 @NgModule({
   declarations: [ButtonComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, I18nModule],
   exports: [ButtonComponent],
 })
 export class ButtonModule {}
